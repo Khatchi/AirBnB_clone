6331eccf8ded886df44d668d9e8896cee5f4115b
Ahavatech <ahavatechng@gmail.com>
2023-08-09 03:17:38 +0100

    test files

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+test file storage module
+
+"""
+
+from json import load, dump, dumps
+from os.path import exists
+
+
+class TestFileStorage():
+    """ Test FileStorage class """
+
+if __name__ == "__main__":
+    unittest.main()
